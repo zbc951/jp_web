502308f391dc0a9d9ebc7783915616aa05f65ce3
lazio@wataidesign.com <lazio@wataidesign.com>
2021-01-26 15:16:56 +0800

    1.26-2

--- js/javascript.js
+++ js/javascript.js
@@ -345,3 +345,14 @@ $(function(){
 		$(this).closest('ul').toggleClass('open');
 	})
 })
+//IE背景抖動問題
+$(function(){
+	if(navigator.userAgent.match(/Trident\/7\./)) {
+		document.body.addEventListener("mousewheel", function() {
+			event.preventDefault();
+			var wd = event.wheelDelta;
+			var csp = window.pageYOffset;
+			window.scrollTo(0, csp - wd);
+		});
+	}
+})
